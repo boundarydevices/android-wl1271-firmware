e5ae941ed9439993018ce6d1eb4fc395ca40d455
Eric Nelson <eric.nelson@boundarydevices.com>
2012-05-18 16:02:57 -0700

    Add Bluetooth bts script

--- Android.mk
+++ Android.mk
@@ -9,4 +9,5 @@ PRODUCT_COPY_FILES += \
 	hardware/wl1271-firmware/ti-connectivity/wl127x-fw-plt-3.bin:system/etc/firmware/ti-connectivity/wl127x-fw-plt-3.bin \
 	hardware/wl1271-firmware/ti-connectivity/wl127x-nvs.bin:system/etc/firmware/ti-connectivity/wl127x-nvs.bin \
 	hardware/wl1271-firmware/ti-connectivity/wl12xx-nvs.bin:system/etc/firmware/ti-connectivity/wl12xx-nvs.bin \
+	hardware/wl1271-firmware/ti-connectivity/TIInit_7.2.31.bts:system/etc/firmware/TIInit_7.2.31.bts \
 
